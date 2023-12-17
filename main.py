db227b2673b2842bd21fd2e7e84ea37d523e28d3
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-17 21:38:08 +0100

    dosent work

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
+import tkinter
 from tkinter import *
 import subprocess
 from tkinter import messagebox
 from os import system
 
-class Main_Window(Frame):
-    def __init__(self, master=None, widget):
-        Frame.__init__(self, master)
-        self.widget = widget
-        self.master = master
-        self.logIn()
-        if (widget == "main_widget"):
-            self.main_widget
-        if(widget == "update_widget"):
-            self.update_widget
+class Main_Window:
+    def __init__(self, root):
+        self.root = root
+        #if (widget == "main_widget"):
+        #    self.main_widget
+        #if(widget == "update_widget"):
+        #   self.update_widget
 
-    def main_widget(self):
-        pass
+    def main_widget(self, root):
+        return root.Label(text = "Willkommen bei OptiLin!")
+
     def update_widget(self):
         pass
 
-widget = "main_widget"
-root = Tk()
-root.geometry("300x200")
+def logIn():
+    try:
+        subprocess.check_call(["sudo", "-S", "apt", "install", "mc", "-y"])
+    except subprocess.CalledProcessError:
+        print("Failed")
+
 def run_app(root, widget):
-    login = logIn()
+    #logIn()
     if(widget == "main_widget"):
-        root.title("Linux Optimization Algorithm")
-        app = Main_Window(root, "main_widget")
+        root.title("OptiLin")
+        app =  Main_Window(root)
+        app.main_widget(root)
     if (widget == "update_widget"):
         root.title("Aktualisierung des Systems")
-        app = Main_Window(root, "update_widget")
+        return Main_Window(root, "update_widget")
 
-    def logIn():
-        try:
-            subprocess.check_call(["sudo", "-S", "apt", "install", "mc", "-y"])
-        except subprocess.CalledProcessError:
-            print("Failed")
         #    messagebox.askokcancel.showinfo("message", "OOOOPS...\nWrong password!")
         # else:
         #   messagebox.askokcancel.showinfo("message", "Login successful!")
+
+widget = "main_widget"
+root = tkinter
+root.geometry("300x200")
+app = None
+app = run_app(root, widget)
+app.pack()
 root.mainloop()
