348452e11bf867c0096f7b3e6e5b36e010fe946a
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-18 23:16:03 +0100

    viva la italia

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
 import tkinter
 from tkinter import *
 import subprocess
-from tkinter import messagebox
 from os import system
+
+class update_widget():
+
+    label = Label (text = "System aktualisieren")
+    button1 = Button(text = "Nach Aktualisierungen suchen")
+    button2 = Button(text = "Mögliche Aktualisierungen installieren")
+    button3 = Button(text = "Pakete herunterladen und installieren")
+
+    window_elements = [label, button1, button2, button3]
+    def show(self):
+        for element in self.window_elements:
+            element.pack()
+        window = tkinter.Tk()
+        window.geometry("1000x1000")
+        window.mainloop()
 
 class main_widget():
 
-    window = Tk()
-    window.geometry("300x200")
-    label = Label(window, text = "Willkommen beim Linux_Optimizer")
-    button1 = Button (window, text = "Aktualisiere dein System")
-    button2 = Button (window, text = "Erneuere deinen App-Speicher")
-    button3 = Button (window, text = "Mach doch was zu willst")
 
-    widgets = [main_widget]
+    label = Label(text = "Willkommen beim Linux_Optimizer")
+    button1 = Button ( text = "Aktualisiere dein System")
+    button2 = Button (text = "Erneuere deinen App-Speicher")
+    button3 = Button (text = "Mach doch was zu willst")
+
+
     window_elements = [label, button1, button2, button3]
-    for element in window_elements:
-        element.pack()
-    window.mainloop()
+
+    def show(self):
+        for element in self.window_elements:
+            element.pack()
+        window = tkinter.Tk()
+        window.geometry("1000x1000")
+        window.mainloop()
+
 
 # start of algorithm
 main = main_widget()
+main.show()
