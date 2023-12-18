7d71a080a9deff2789a7516ffefe1ab369d62462
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-18 22:43:41 +0100

    new structure

--- main.py
+++ main.py
@@ -4,44 +4,20 @@ import subprocess
 from tkinter import messagebox
 from os import system
 
-class Main_Window:
-    def __init__(self, root):
-        self.root = root
-        #if (widget == "main_widget"):
-        #    self.main_widget
-        #if(widget == "update_widget"):
-        #   self.update_widget
+class main_widget():
 
-    def main_widget(self, root):
-        return root.Label(text = "Willkommen bei OptiLin!")
+    window = Tk()
+    window.geometry("300x200")
+    label = Label(window, text = "Willkommen beim Linux_Optimizer")
+    button1 = Button (window, text = "Aktualisiere dein System")
+    button2 = Button (window, text = "Erneuere deinen App-Speicher")
+    button3 = Button (window, text = "Mach doch was zu willst")
 
-    def update_widget(self):
-        pass
+    widgets = [main_widget]
+    window_elements = [label, button1, button2, button3]
+    for element in window_elements:
+        element.pack()
+    window.mainloop()
 
-def logIn():
-    try:
-        subprocess.check_call(["sudo", "-S", "apt", "install", "mc", "-y"])
-    except subprocess.CalledProcessError:
-        print("Failed")
-
-def run_app(root, widget):
-    #logIn()
-    if(widget == "main_widget"):
-        root.title("OptiLin")
-        app =  Main_Window(root)
-        app.main_widget(root)
-    if (widget == "update_widget"):
-        root.title("Aktualisierung des Systems")
-        return Main_Window(root, "update_widget")
-
-        #    messagebox.askokcancel.showinfo("message", "OOOOPS...\nWrong password!")
-        # else:
-        #   messagebox.askokcancel.showinfo("message", "Login successful!")
-
-widget = "main_widget"
-root = tkinter
-root.geometry("300x200")
-app = None
-app = run_app(root, widget)
-app.pack()
-root.mainloop()
+# start of algorithm
+main = main_widget()
