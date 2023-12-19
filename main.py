9ee7cdb8df235e32ff7e577329772d90053266ad
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-19 21:12:42 +0100

    fixed window problem

--- main.py
+++ main.py
@@ -1,42 +1,44 @@
-import tkinter
-from tkinter import *
-import subprocess
-from os import system
+import tkinter as tk
+from tkinter import ttk
 
+''''''
 class update_widget():
 
-    label = Label (text = "System aktualisieren")
-    button1 = Button(text = "Nach Aktualisierungen suchen")
-    button2 = Button(text = "Mögliche Aktualisierungen installieren")
-    button3 = Button(text = "Pakete herunterladen und installieren")
+    def __init__(self):
 
-    window_elements = [label, button1, button2, button3]
+        label = tk.Label (text = "System aktualisieren")
+        button1 = tk.Button(text = "Nach Aktualisierungen suchen")
+        button2 = tk.Button(text = "Mögliche Aktualisierungen installieren")
+        button3 = tk.Button(text = "Pakete herunterladen und installieren")
+
+        window_elements = [label, button1, button2, button3]
     def show(self):
         for element in self.window_elements:
             element.pack()
-        window = tkinter.Tk()
+        window = tk.Tk()
+        window.title("Hello from the dark side")
         window.geometry("1000x1000")
         window.mainloop()
+
+class another_widget:
+    pass
 
 class main_widget():
 
+    def __init__(self):
 
-    label = Label(text = "Willkommen beim Linux_Optimizer")
-    button1 = Button ( text = "Aktualisiere dein System")
-    button2 = Button (text = "Erneuere deinen App-Speicher")
-    button3 = Button (text = "Mach doch was zu willst")
+        root = tk.Tk()
+        root.title("Hello")
+        label = tk.Label(root, text="Willkommen beim Linux_Optimizer")
+        button1 = tk.Button(root, text="Aktualisiere dein System")
+        button2 = tk.Button(root, text="Erneuere deinen App-Speicher")
+        button3 = tk.Button(root, text="Mach doch was zu willst")
+        window_elements = [label, button1, button2, button3]
+        for i in window_elements:
+            i.pack()
+        root.mainloop()
 
-
-    window_elements = [label, button1, button2, button3]
-
-    def show(self):
-        for element in self.window_elements:
-            element.pack()
-        window = tkinter.Tk()
-        window.geometry("1000x1000")
-        window.mainloop()
 
 
 # start of algorithm
-main = main_widget()
-main.show()
+app = main_widget()
