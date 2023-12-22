057f00923ebc7f5b00de27e1e2646613fdfe1211
JustForStephan <62476088+JustForStephan@users.noreply.github.com>
2023-12-22 23:41:38 +0100

    updates are possible

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 import tkinter as tk
-from tkinter import ttk
+import subprocess
+import os
 
 ''''''
 class update_widget():
+
+    def update_sys(self):
+        os.system("apt update")
+        os.system("apt upgrade -y")
+        os.system("apt clean")
+        os.system("apt autoremove -y")
 
     def __init__(self):
         root_update = tk.Tk()
@@ -11,8 +18,8 @@ class update_widget():
         label2 = tk.Label(root_update, text = "   1. Neue Aktualisierungen werden gesucht und installiert.")
         label3 = tk.Label(root_update, text = "   2. Der Aktualisierungschache wird geleert.")
         label4 = tk.Label(root_update, text = "   3. Alle überflüssigen Pakete werden aus dem System gelöscht.")
-        label5 = tk.Label(root_update, text = "Möchten Sie eine vollständige Systemaktualisierung durchführen?", bg='#fff')
-        window_labels = [label1, label2, label3, label4, label5]
+        button_dec = tk.Button(root_update, text = "Ich möchte eine vollständige Systemaktualisierung durchführen", command = lambda: self.update_sys())
+        window_labels = [label1, label2, label3, label4, button_dec]
         for i in window_labels:
             i.pack()
         root_update.mainloop()
@@ -28,8 +35,14 @@ class main_widget():
     def actualize_sys(self):
         update_widget()
 
-    def __init__(self):
+    def login(self):
+        try:
+            subprocess.check_call(["sudo", "-S", "apt", "install", "mc", "-y"])
+        except subprocess.CalledProcessError:
+            print("Failed")
 
+    def __init__(self):
+        self.login()
         root = tk.Tk()
         root.title("Linux_Optimizer")
         label = tk.Label(root, text="Willkommen beim Linux_Optimizer")
